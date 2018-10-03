a05b46e94496cff583ee527cae6ba990ebddf74f
Vince Andrew Pimentel <contact@vincep.me>
2018-10-03 07:42:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(array, index)
   if index.between?(0, 8) == true || position_taken? == false
     true
-  elsif index < 0 == true || index > 8 == true
+  elsif index.between?(0, 8) == false
     false
   elsif position_taken? == true
     false
